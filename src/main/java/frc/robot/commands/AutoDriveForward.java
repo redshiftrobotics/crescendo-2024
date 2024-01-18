9a262ea720b13a8aeec58276d4142419a85a7b42
Aceius E <aceiuselms@seattleacademy.org>
2024-01-17 16:26:17 -0800

    Update AutoDriveForward so it can stop
    Yes it did build
    I have no idea if it works

--- src/main/java/frc/robot/commands/AutoDriveForward.java
+++ src/main/java/frc/robot/commands/AutoDriveForward.java
@@ -5,14 +5,10 @@ import frc.robot.subsystems.SwerveDrivetrain;
 
 public class AutoDriveForward extends Command {
     private final SwerveDrivetrain drivetrain;
-    private boolean finishedDriving = false;
+    private long beganDriving;
 
     /**
-     * The constructor creates a new command and is automatically called one time when the command is created (with 'new' keyword).
-     * It should set up the initial state and properties of the object to ensure it's ready for use.
-     * This can take in any arguments you need. It normally uses 1 subsystem (but an take multiple when necessary),
-     * as wells as arguments for what to do, such as a joystick in the drive command or a desired position in an auto command.
-     * Example uses include saving parameters passed to the command, creating and configuring objects for the class like PID controllers, and adding subsystem requirements
+     * Inject dependencies and reserve systems
      */
     public AutoDriveForward(SwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
@@ -21,44 +17,31 @@ public class AutoDriveForward extends Command {
     }
 
     /**
-     * initialize() is used to prepare a command for execution and is called once when the command is scheduled.
-     * It should reset the command's state since command objects can be used multiple times.
-     * Example uses include setting motor to constant speed, setting a solenoid to a certain state, and resetting variables
+     * Begin moving
      */
     @Override
     public void initialize() {
         ChassisSpeeds desiredState = new ChassisSpeeds(0.5,0,0);
+        this.beganDriving = System.currentTimeMillis();
 
         drivetrain.setDesiredState(desiredState);
     }
 
-    /**
-     * execute() is called repeatedly while a command is scheduled, about every 20ms.
-     * It should handle continuous tasks specific to the command, like updating motor outputs based on joystick inputs or utilizing control loop results.
-     * Example uses include adjusting motor speeds for real-time control, processing sensor data within a scheduled command, and using the output of a control loop.
-     */
-    @Override
-    public void execute() {
-    }
+    // No execute() because we don't need it
 
     /**
-     * isFinished() finished is called repeatedly while a command is scheduled, right after execute.
-     * It should return true when you want the command to finish. end(false) is called directly after isFinished() returns true.
-     * Example uses include checking if control loop is at set point, and always returning false to end after just 1 call to execute.
+     * Did we begin driving 10 or more seconds ago? If so trigger the finished condition and stop the robot.
      */
     @Override
     public boolean isFinished() {
-        return this.finishedDriving;
+        return (beganDriving + 10000) >= System.currentTimeMillis();
     }
 
     /**
-     * end(boolean interrupted) is called once when a command ends, regardless of whether it finishes normally or is interrupted.
-     * It should wrap up the command since other commands might use the same subsystems.
-     * Once end runs the command will no longer be in the command scheduler loop.
-     * It takes in a boolean interrupted which is set to true when the command is ended without isFinished() returning true.
-     * Example uses include setting motor speeds back to zero, and setting a solenoid back to a "default" state.
+     * Turn off motors when we don't want the robot to move anymore
      */
     @Override
     public void end(boolean interrupted) {
+        drivetrain.stop();
     }
 }
