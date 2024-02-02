cf0ace8cbf98f98cc40c8988300eca040acd4df6
Zephyr <55262287+Zr6573@users.noreply.github.com>
2024-02-01 16:31:39 -0800

    Update Hanger.java

--- src/main/java/frc/robot/subsystems/Hanger.java
+++ src/main/java/frc/robot/subsystems/Hanger.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -11,12 +12,14 @@ public class Hanger extends SubsystemBase {
     
     private final CANSparkMax hangerMotor;
     private final CANcoder hangerEncoder;
+    private final PIDController hangerPIDController;
     int hangerMotorID;
     int hangerEncoderID;
     
-    public Hanger() {
+    public Hanger(int hangerMotorID, int hangerEncoderID) {
         hangerMotor = new CANSparkMax(hangerMotorID, MotorType.kBrushless);
         hangerEncoder = new CANcoder(hangerEncoderID);
+        hangerPIDController = new PIDController(0, 0, 0);
     }
 
     @Override
