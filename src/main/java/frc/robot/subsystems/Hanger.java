f1de270d61cf3f3948a4e47d2865ba61b19d1167
Zephyr <55262287+Zr6573@users.noreply.github.com>
2024-02-01 16:40:12 -0800

    Update Hanger.java

--- src/main/java/frc/robot/subsystems/Hanger.java
+++ src/main/java/frc/robot/subsystems/Hanger.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -13,13 +14,22 @@ public class Hanger extends SubsystemBase {
     private final CANSparkMax hangerMotor;
     private final CANcoder hangerEncoder;
     private final PIDController hangerPIDController;
+    private final StatusSignal<Double> hangerPosition;
+
     int hangerMotorID;
     int hangerEncoderID;
+    int p;
+    int i;
+    int d;
     
     public Hanger(int hangerMotorID, int hangerEncoderID) {
+
         hangerMotor = new CANSparkMax(hangerMotorID, MotorType.kBrushless);
         hangerEncoder = new CANcoder(hangerEncoderID);
-        hangerPIDController = new PIDController(0, 0, 0);
+
+        hangerPIDController = new PIDController(p, i, d);
+
+        hangerPosition = hangerEncoder.getAbsolutePosition();
     }
 
     @Override
