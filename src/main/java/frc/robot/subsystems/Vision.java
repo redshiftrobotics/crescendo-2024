9996b906592e8c4db0e8018c79bdf68d21ad468c
DigitalFork <113265535+DigitalFork@users.noreply.github.com>
2024-02-03 10:27:24 -0800

    Add Initial Full Vision Pipeline Test Add getDist methods for tag detection in bot. Add ShuffleBoard field for posting tag numbers. (Not working, need to test)

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,6 +1,14 @@
 package frc.robot.subsystems;
 
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 // How to make Subsystem (ignore image instructions, code is out of date, just look at written general instructions): https://compendium.readthedocs.io/en/latest/tasks/subsystems/subsystems.html
 // Command based programming: https://docs.wpilib.org/en/stable/docs/software/commandbased/what-is-command-based.html
@@ -11,12 +19,45 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * 1. Copy this file, remember that class name has to match 
  */
 
-public class ExampleSubsystem extends SubsystemBase {
+public class Vision extends SubsystemBase {
     /** Constructor. Creates a new ExampleSubsystem. */
-    public ExampleSubsystem() {
+    PhotonCamera camera;
+    
+    
+    PhotonTrackedTarget bestTag = null;
+    List<PhotonTrackedTarget> targets = null;
+    
+    public Vision(String cameraName) {
+        camera = new PhotonCamera(cameraName);
 
     }
 
+    /**
+     * Gives Transform3d from the robot center to the desired target
+     * @return
+     */
+    public Transform3d getDistToTag() {
+        return bestTag.getBestCameraToTarget();
+    }
+
+    /**
+     * Gives Transform3d from robot center to the desired target
+     * @param tagID The fiducial ID of the desired April Tag
+     * @return Returns null if the tag cannot be found
+     * 
+     */
+    public Transform3d getDistToTag(int tagID) {
+        Transform3d dist = null;
+        for(int i = 0; i < targets.size(); i++)
+        {
+            if (targets.get(i).getFiducialId() == tagID) {
+                dist = targets.get(i).getBestCameraToTarget();
+                break;
+            }
+        }
+        
+        return dist;
+    }
     /**
      * This method is called periodically by the CommandScheduler, about every 20ms.
      * It should be used for updating subsystem-specific state that you don't want to offload to a Command.
@@ -24,6 +65,16 @@ public class ExampleSubsystem extends SubsystemBase {
      */
     @Override
     public void periodic() {
+        PhotonPipelineResult result = camera.getLatestResult();
         // This method will be called once per scheduler run
+        bestTag = result.getBestTarget();
+        targets = result.getTargets();
+        if(bestTag != null) {
+            SmartDashboard.putNumber("Tag ID", bestTag.getFiducialId());
+        } else {
+            SmartDashboard.putNumber("Tag ID", -1);
+        }
+
+        
     }
 }
