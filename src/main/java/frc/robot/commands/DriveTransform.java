ba6cff6847f1d04b39a403d281edbede98047880
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-02-05 16:58:35 -0800

    Rewrite some new DriveToPose and create

--- src/main/java/frc/robot/commands/DriveTransform.java
+++ src/main/java/frc/robot/commands/DriveTransform.java
@@ -4,13 +4,10 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.SwerveDrivetrain;
 
 /** Command to automatically drive a certain transform */
-public class DriveTransform extends Command {
-    private final SwerveDrivetrain drivetrain;
+public class DriveTransform extends DriveToPoseBase {
     private final Transform2d transform;
 
     /**
@@ -19,11 +16,11 @@ public class DriveTransform extends Command {
      * <p>This drives relative to the robot, so a transform of +2x and +1y will drive 2 meters forward and 1 meter left,
      * where forward is whatever direction the robot is currently facing</p>
      * 
-     * @param drivetrain The drivetrain of the robot
-     * @param transform Target transform to drive to, will be added to current position to get target pose
+     * @param drivetrain the drivetrain of the robot
+     * @param transform target transform to drive to, will be added to current position to get target pose
      */
     public DriveTransform(SwerveDrivetrain drivetrain, Transform2d transform) {
-        this.drivetrain = drivetrain;
+        super(drivetrain);
         this.transform = transform;
     }
     
@@ -43,14 +40,6 @@ public class DriveTransform extends Command {
 
     @Override
     public void initialize() {
-        CommandScheduler.getInstance().schedule(
-            new DriveToPose(drivetrain, drivetrain.getPosition().plus(transform))
-        );
-    }
-
-    @Override
-    public boolean isFinished() {
-        // Instantly finish since the entire command just calls another command 
-        return true;
+        setDesiredPosition(getPosition().plus(transform));
     }
 }
--- src/main/java/frc/robot/commands/FollowTag.java
+++ src/main/java/frc/robot/commands/FollowTag.java
@@ -1,47 +1,69 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.RobotMovementConstants;
 import frc.robot.subsystems.SwerveDrivetrain;
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.Constants.RobotMovementConstants;
 
-public class FollowTag extends Command {
-    private final SwerveDrivetrain drivetrain;
+/** Command to automatically drive a follow a tag a certain translation away */
+public class FollowTag extends DriveToPoseBase {
     private final int tagID;
-    private final Translation2d targetDistance;
-    private final PIDController xController, yController, rotationController;
+    private final Transform2d targetDistance;
 
-    public FollowTag(SwerveDrivetrain drivetrain, int tagID, Translation2d targetDistanceToTag) {
-        this.drivetrain = drivetrain;
+    private final double loseTagAfterSeconds;
+    private double secondsSinceTagLastSeen;
+
+    /**
+     * Create a new FollowTag command. Tries to follow a tag while staying a certain distance away.
+     * 
+     * @param drivetrain the drivetrain of the robot
+     * @param tagID the numerical ID of the the tag to follow
+     * @param targetDistanceToTag the target distance away from the tag to be
+     * @param loseTagAfterSeconds how long to wait before giving up on rediscover tag, set to -1 to never finish
+     */
+    public FollowTag(SwerveDrivetrain drivetrain, int tagID, Transform2d targetDistanceToTag, int loseTagAfterSeconds) {
+        super(drivetrain);
+
         this.tagID = tagID;
         this.targetDistance = targetDistanceToTag;
-
-        xController = new PIDController(
-            RobotMovementConstants.TRANSLATION_PID_P,
-            RobotMovementConstants.TRANSLATION_PID_I,
-            RobotMovementConstants.TRANSLATION_PID_D
-        );
-        xController.setTolerance(RobotMovementConstants.POSITION_TOLERANCE_METERS);
-
-        yController = new PIDController(
-            RobotMovementConstants.TRANSLATION_PID_P,
-            RobotMovementConstants.TRANSLATION_PID_I,
-            RobotMovementConstants.TRANSLATION_PID_D
-        );
-        yController.setTolerance(RobotMovementConstants.POSITION_TOLERANCE_METERS);
-
-        rotationController = new PIDController(
-            RobotMovementConstants.ROTATION_PID_P,
-            RobotMovementConstants.ROTATION_PID_I,
-            RobotMovementConstants.ROTATION_PID_D
-        );
-        rotationController.setTolerance(RobotMovementConstants.ANGLE_TOLERANCE_RADIANS);
+        this.loseTagAfterSeconds = loseTagAfterSeconds;
+    }
+    
+    /**
+     * Create a new FollowTag command. Tries to follow a tag while staying a certain distance away.
+     * 
+     * @param drivetrain the drivetrain of the robot
+     * @param tagID the numerical ID of the the tag to follow
+     * @param targetDistanceToTag the target distance away from the tag to be
+     * @param loseTagAfterSeconds how long to wait before giving up on rediscover tag, set to -1 to never finish
+     */
+    public FollowTag(SwerveDrivetrain drivetrain, int tagID, Translation2d targetDistanceToTag, int loseTagAfterSeconds) {
+        this(drivetrain, tagID, new Transform2d(targetDistanceToTag, new Rotation2d()), loseTagAfterSeconds);
     }
 
     @Override
     public void execute() {
-        
+
+        // Sudo code, assume distance from front center of robot
+        final Transform3d tagPosition3d = new Transform3d(tagID * 5, 0, 0, new Rotation3d());
+
+        // https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/coordinate-systems.html
+        // https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/geometry/Rotation3d.html#getZ()
+        final Transform2d tagPosition = new Transform2d(
+            tagPosition3d.getZ(),
+            tagPosition3d.getX(),
+            Rotation2d.fromRadians(tagPosition3d.getRotation().getZ())
+        );
+
+        final Transform2d driveTransform = tagPosition.plus(targetDistance.inverse());
+
+        setDesiredPosition(getPosition().plus(driveTransform));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (loseTagAfterSeconds != -1) && (secondsSinceTagLastSeen < loseTagAfterSeconds);
     }
 }
