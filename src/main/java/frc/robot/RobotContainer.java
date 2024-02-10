4278680a65958a87ad3db5c62caaf9a202e4d62b
DigitalFork <113265535+DigitalFork@users.noreply.github.com>
2024-02-10 13:47:30 -0800

    Squashed commit of the following:
    commit e8f587211f3846ad5cef366dc51678cd3f9ca8b5
    Merge: 9d2afd0 9b3b533
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 10 11:53:01 2024 -0800
    
        Merge branch 'main' into vision
    
    commit 9d2afd0389eb62be272a461a5e52da90f23a5b4c
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 10 11:42:59 2024 -0800
    
        Re-add commented lines (for merge)
    
    commit eb7c2c1a87d7a1963a8dc3a5170461c5f67387a0
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 10 11:34:40 2024 -0800
    
        Add camera name constant and port forwarder for USB port on RoboRIO
        https://docs.photonvision.org/en/latest/docs/installation/networking.html#port-forwarding
    
    commit 91d13c34e427cd716fd1ce4a1e8a79319a1f73ec
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Wed Feb 7 21:00:11 2024 -0800
    
        Update PhotonLib version
        Fix SmartDashboard put statement
        Add camera offset transform constant
    
    commit bce9f628b776321886977f7c944afbd72ea52188
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 3 21:57:36 2024 -0800
    
        Add new post statements for testing
        Need to get 3D AprilTags working
    
    commit 00ddc23776d051a9eb17bf2a23d606034028fac8
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 3 14:11:35 2024 -0800
    
        VISION PIPELINE WORKS!
        Update photonlib vendordep
        Vision cameraName is Arducam name
    
    commit 9996b906592e8c4db0e8018c79bdf68d21ad468c
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 3 10:27:24 2024 -0800
    
        Add Initial Full Vision Pipeline Test
        Add getDist methods for tag detection in bot.
        Add ShuffleBoard field for posting tag numbers. (Not working, need to test)
    
    commit 2e38015b6a248c65732e0afbbc08cd5e80ae5f09
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Wed Jan 24 15:50:17 2024 -0800
    
        Add initial files for vision

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,13 @@
 package frc.robot;
 
 import frc.robot.Constants.DriverConstants;
+import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.SwerveDrivetrainConstants;
 import frc.robot.Constants.SwerveModuleConstants;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.SwerveDrivetrain;
+import frc.robot.subsystems.SwerveModule;
+import frc.robot.subsystems.Vision;
 import frc.robot.commands.Autos;
 import frc.robot.commands.DriverControl;
 import frc.robot.subsystems.SwerveDrivetrain;
@@ -19,6 +24,7 @@ import edu.wpi.first.wpilibj.GenericHID.HIDType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
@@ -26,8 +32,11 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
  * This class is where the bulk of the robot should be declared.
- * Since Command-based is a "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
- * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ * Since Command-based is a "declarative" paradigm, very little robot logic
+ * should actually be handled in the {@link Robot} periodic methods (other than
+ * the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and
+ * trigger mappings) should be declared here.
  */
 public class RobotContainer {
 
@@ -68,6 +77,8 @@ public class RobotContainer {
 
 	private final SendableChooser<Command> autoChooser = new SendableChooser<Command>();
 
+	private final Vision vision = new Vision(VisionConstants.CAMERA_NAME);
+
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
 	 */
@@ -78,6 +89,8 @@ public class RobotContainer {
 		configureBindings();
 
 		setUpDriveController();
+
+		PortForwarder.add(5800, "photonvision.local", 5800);
 	}
 
 	public void setUpDriveController() {
@@ -96,30 +109,27 @@ public class RobotContainer {
 			final CommandJoystick joystick = new CommandJoystick(genericHID.getPort());
 			control = new DriverControl(drivetrain,
 
-				new ChassisDriveInputs(
-					joystick::getX, joystick::getY, joystick::getTwist,
-					-1, -1, Constants.DriverConstants.DEAD_ZONE),
+					new ChassisDriveInputs(
+							joystick::getX, joystick::getY, joystick::getTwist,
+							-1, -1, Constants.DriverConstants.DEAD_ZONE),
 
-				new OptionButton(joystick, 2, ActivationMode.TOGGLE),
-				new OptionButton(joystick, 1, ActivationMode.HOLD),
-				new OptionButton(joystick, 3, ActivationMode.TOGGLE)
-			);
-			
+					new OptionButton(joystick, 2, ActivationMode.TOGGLE),
+					new OptionButton(joystick, 1, ActivationMode.HOLD),
+					new OptionButton(joystick, 3, ActivationMode.TOGGLE));
+
 			joystick.button(4).onTrue(Commands.run(drivetrain::brakeMode, drivetrain));
 
 		} else {
 			final CommandXboxController xbox = new CommandXboxController(genericHID.getPort());
 			control = new DriverControl(drivetrain,
 
-				new ChassisDriveInputs(
-					xbox::getLeftX, xbox::getLeftY, xbox::getRightX,
-					+1, -1, Constants.DriverConstants.DEAD_ZONE),
+					new ChassisDriveInputs(
+							xbox::getLeftX, xbox::getLeftY, xbox::getRightX,
+							+1, -1, Constants.DriverConstants.DEAD_ZONE),
 
-				new OptionButton(xbox::b, ActivationMode.TOGGLE),
-				new OptionButton(xbox::leftStick, ActivationMode.HOLD),
-				new OptionButton(xbox::povUp, ActivationMode.TOGGLE)
-			);
-
+					new OptionButton(xbox::b, ActivationMode.TOGGLE),
+					new OptionButton(xbox::leftStick, ActivationMode.HOLD),
+					new OptionButton(xbox::povUp, ActivationMode.TOGGLE));
 
 		}
 
