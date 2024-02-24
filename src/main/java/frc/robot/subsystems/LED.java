5b38c8cd499e00c9a0f9d079611fd27f1306530c
RamiRedShift <155695553+RamiRedShift@users.noreply.github.com>
2024-02-24 11:44:50 -0800

    Update LED.java

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -16,20 +16,17 @@ public class LED {
 		// seting the data
 		m_led.setData(m_ledBuffer.getLength());
 		m_led.start();
-	}
 
-	for(
-
-	var i = 0;i<m_ledBuffer.getLength();i++)
-	{
-		// looping between red and black
-		while (true) {
-			m_ledBuffer.setHSV(i, 1, 100, 64);
-			wait(5);
-			m_ledBuffer.setHSV(i, 0, 0, 0);
-			wait(5);
+		for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+			// looping between red and black
+			while (true) {
+				m_ledBuffer.setHSV(i, 1, 100, 64);
+				wait(5);
+				m_ledBuffer.setHSV(i, 0, 0, 0);
+				wait(5);
+			}
 		}
-	}
 
-	m_led.setData(m_ledBuffer);
+		m_led.setData(m_ledBuffer);
+	}
 }
