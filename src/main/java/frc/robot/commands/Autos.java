2774f8f4ae0817c90bef7c8c671b87159c96ac70
Aleah H <aleahjham@gmail.com>
2024-02-24 12:23:26 -0800

    Make the auto choosable through smartdashboard

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -2,12 +2,25 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.Vision;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.SwerveDrivetrainConstants;
 import frc.robot.subsystems.Arm;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 /**
  * This class just contains a bunch of auto-modes. Do not call this class
@@ -71,5 +84,34 @@ public final class Autos {
 
 	private Autos() {
 		throw new UnsupportedOperationException("This is a utility class!");
+	}
+
+
+
+
+
+	public static Command trajectoryAuto(SwerveDrivetrain drivetrain){
+
+
+		// Trajectory Config
+		final TrajectoryConfig exampleConfig = new TrajectoryConfig(AutoConstants.kMaxAutoVelocitySpeedMetersPerSecond,
+					AutoConstants.kMaxAutoRotationSpeedMetersPerSecond).setKinematics(drivetrain.getKinematics());
+		// Example Trajectory (1 meter forward then backward)
+		final Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
+					List.of(new Translation2d(0, 0.5)), new Pose2d(0, 1, new Rotation2d(0)), exampleConfig);
+		// Profiled PID Controller for trajectory rotation
+		final ProfiledPIDController rotationPidController = new ProfiledPIDController(AutoConstants.kAngularControllerP,
+					0, 0, AutoConstants.kRotationControllerConstraints);
+		rotationPidController.enableContinuousInput(-Math.PI, Math.PI);
+		// SwerveControllerCommand Test (Trajectory Auto Drive)
+		final SwerveControllerCommand trajectoryTestCommand = new SwerveControllerCommand(exampleTrajectory,
+					drivetrain::getPosition, drivetrain.getKinematics(),
+					new PIDController(AutoConstants.kVelocityControllerP, 0, 0),
+					new PIDController(AutoConstants.kVelocityControllerP, 0, 0), rotationPidController,
+					drivetrain::setSwerveModuleStates, drivetrain);
+
+		return Commands.sequence(new InstantCommand(() -> drivetrain.resetPosition()), trajectoryTestCommand,
+					new InstantCommand(() -> drivetrain.setDesiredState(new ChassisSpeeds(0, 0, 0))));
+
 	}
 }
