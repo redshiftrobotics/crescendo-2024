250fb5ee1658af61f0ee369978c83a0698cad295
Aceius E <aceiuselms@seattleacademy.org>
2024-02-26 15:52:58 -0800

    Always fix everything

--- src/main/java/frc/robot/commands/FaceTag.java
+++ src/main/java/frc/robot/commands/FaceTag.java
@@ -1,4 +1,5 @@
 package frc.robot.commands;
+
 import java.util.Map;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -8,30 +9,34 @@ import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.Vision;
 
 public class FaceTag extends Command {
-    private final Vision vision;
-    private final SwerveDrivetrain drivetrain;
-    private final PIDController rotationPID;
+	private final Vision vision;
+	private final SwerveDrivetrain drivetrain;
+	private final PIDController rotationPID;
 
-    public FaceTag(Vision vision, SwerveDrivetrain drivetrain, Map<Integer,Double> targetTagPos){
-        this.vision = vision;
-        this.drivetrain = drivetrain;
-        rotationPID = new PIDController(
-            RobotMovementConstants.ROTATION_PID_P,
-            RobotMovementConstants.ROTATION_PID_I,
-            RobotMovementConstants.ROTATION_PID_D);
-        rotationPID.enableContinuousInput(-Math.PI, Math.PI);
-        rotationPID.setTolerance(RobotMovementConstants.ANGLE_TOLERANCE_RADIANS);
+	public FaceTag(Vision vision, SwerveDrivetrain drivetrain, Map<Integer, Double> targetTagPos) {
+		this.vision = vision;
+		this.drivetrain = drivetrain;
+		rotationPID = new PIDController(
+				RobotMovementConstants.ROTATION_PID_P,
+				RobotMovementConstants.ROTATION_PID_I,
+				RobotMovementConstants.ROTATION_PID_D);
+		rotationPID.enableContinuousInput(-Math.PI, Math.PI);
+		rotationPID.setTolerance(RobotMovementConstants.ANGLE_TOLERANCE_RADIANS);
 
-        addRequirements(drivetrain);
-    }
+		addRequirements(drivetrain);
+	}
 
-    @Override
-    public void initialize(){
-        drivetrain.toDefaultStates();
-    }
+	@Override
+	public void initialize() {
+		drivetrain.toDefaultStates();
+	}
 
-    @Override
-    public void execute(){   
-    }
+	public void a(String amog) {
+		return;
+	}
+
+	@Override
+	public void execute() {
+	}
 
 }
