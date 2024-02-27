2717dff9fc2119fca4b39a36315f3d07389c41a6
Aceius E <aceiuselms@seattleacademy.org>
2024-02-26 17:35:59 -0800

    Format document

--- src/main/java/frc/robot/commands/LED.java
+++ src/main/java/frc/robot/commands/LED.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class LED extends Command{
+public class LED extends Command {
 
 	private AddressableLED m_led;
 	private AddressableLEDBuffer m_ledBuffer;
@@ -12,14 +12,27 @@ public class LED extends Command{
 	private int s = 0;
 	private int v = 0;
 
+	private void setColor(int h, int s, int v) {
+		for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+			m_ledBuffer.setHSV(i, h, s, v);
+		}
+	}
+	
+	public LED(AddressableLED m_led, AddressableLEDBuffer m_ledBuffer, int h, int s, int v) {
+		this.m_led = m_led;
+		this.m_ledBuffer = m_ledBuffer;
+		this.h = h;
+		this.s = s;
+		this.v = v;
+	}
+
 	@Override
 	public void initialize() {
-		
 		// 9 is the PWM port
 		// m_led = new AddressableLED(9);
-
 		// Default length of 60 starting with empty output
 		// m_ledBuffer = new AddressableLEDBuffer(60);
+
 		m_led.setLength(m_ledBuffer.getLength());
 
 		// seting the data
@@ -30,23 +43,4 @@ public class LED extends Command{
 
 		m_led.setData(m_ledBuffer);
 	}
-
-	public void setColor(int h, int s, int v) {
-		for (int i = 0; i < m_ledBuffer.getLength(); i++) {
-			m_ledBuffer.setHSV(i, h, s, v);
-		}
-	}
-
-	public LED(AddressableLED m_led, AddressableLEDBuffer m_ledBuffer, int h, int s, int v) {
-
-		this.m_led = m_led;
-		this.m_ledBuffer = m_ledBuffer;
-		this.h = h;
-		this.s = s;
-		this.v = v;
-
-	}
-
-
-
 }
