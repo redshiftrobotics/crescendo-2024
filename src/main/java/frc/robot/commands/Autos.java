33bd25f3962bb68b551f5db42d2f172b72a4fe2c
DigitalFork <113265535+DigitalFork@users.noreply.github.com>
2024-03-13 15:05:31 -0700

    Make theoretically functional 3-note auto

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -9,6 +9,7 @@ import frc.robot.subsystems.intake.IntakeShooter;
 import java.util.Optional;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -27,7 +28,14 @@ import frc.robot.subsystems.hang.Hang;
  * itself.
  */
 public final class Autos {
-	private static final Translation2d rightNotePickup = new Translation2d((Constants.BOT_WIDTH / 2) - Units.inchesToMeters(77.875/2), Units.inchesToMeters(39));
+	/**
+	 * The translation to the preliminary position for third note pickup
+	 * <p>
+	 * Relative to the robot which will be facing away from the speaker
+	 */
+	private static final Translation2d rightNotePickup = new Translation2d(
+			-(Constants.BOT_WIDTH / 2) - Units.inchesToMeters(77.875 / 2), Units.inchesToMeters(39));
+	private static final double speakerDepth = Units.inchesToMeters(36.125);
 
 	/** Example static factory for an autonomous command. */
 	public static Command driveAuto(SwerveDrivetrain drivetrain, Translation2d translation) {
@@ -57,49 +65,63 @@ public final class Autos {
 	}
 
 	/**
-	 * This factory creates a new command sequence that picks shoots a preloaded note and picks up another one before shooting it as well.
+	 * This factory creates a new command sequence that picks shoots a preloaded
+	 * note and picks up another one before shooting it as well.
 	 *
 	 * It also pulls both hangers down.
 	 *
 	 * @author Linden, Aceius E.
-	 * @param drivetrain  The robot drivetrain
-	 * @param arm         The main arm
-	 * @param shooter     The shooter
-	 * @param leftHang    The left hanger
-	 * @param rightHang   The right hanger
+	 * @param drivetrain The robot drivetrain
+	 * @param arm        The main arm
+	 * @param shooter    The shooter
+	 * @param leftHang   The left hanger
+	 * @param rightHang  The right hanger
 	 * @return Command schedule instructions
 	 */
-	public static Command shoot2StartingAuto(SwerveDrivetrain drivetrain, Arm arm, IntakeShooter shooter, Hang leftHang, Hang rightHang) {
+	public static Command shoot2StartingAuto(SwerveDrivetrain drivetrain, Arm arm, IntakeShooter shooter, Hang leftHang,
+			Hang rightHang) {
 		final double driveDistanceForNote1 = -1.59;
 
 		return Commands.parallel(
-			Commands.sequence(
-					shootInSpeaker(drivetrain, arm, shooter, null, null),
-					new ArmRotateTo(arm, ArmConstants.ARM_STOW_2_DEGREES),
-					Commands.parallel(
-							new AutoDriveTo(drivetrain, new Translation2d(driveDistanceForNote1, 0)),
-							new SpinIntakeGrabbers(shooter, IntakeShooterConstants.INTAKE_GRABBER_SPEED_SPEAKER)
-					),
-					new SpinIntakeGrabbers(shooter, 0),
-					new AutoDriveTo(drivetrain, new Translation2d(-driveDistanceForNote1, 0)),
-					shootInSpeaker(drivetrain, arm, shooter, null, null)
-			),
-			new PullHangerDown(rightHang, HangConstants.SPEED),
-			new PullHangerDown(leftHang, HangConstants.SPEED));
+				Commands.sequence(
+						shootInSpeaker(drivetrain, arm, shooter, null, null),
+						new ArmRotateTo(arm, ArmConstants.ARM_STOW_2_DEGREES),
+						Commands.parallel(
+								new AutoDriveTo(drivetrain, new Translation2d(driveDistanceForNote1, 0)),
+								new SpinIntakeGrabbers(shooter, IntakeShooterConstants.INTAKE_GRABBER_SPEED_SPEAKER)),
+						new SpinIntakeGrabbers(shooter, 0),
+						new AutoDriveTo(drivetrain, new Translation2d(-driveDistanceForNote1, 0)),
+						shootInSpeaker(drivetrain, arm, shooter, null, null)),
+				new PullHangerDown(rightHang, HangConstants.SPEED),
+				new PullHangerDown(leftHang, HangConstants.SPEED));
 	}
 
-	public static Command shoot3StartingAuto(SwerveDrivetrain drivetrain, Arm arm, IntakeShooter shooter, Vision vision, Hang leftHanger, Hang rightHanger) throws Exception {
+	public static Command shoot3StartingAuto(SwerveDrivetrain drivetrain, Arm arm, IntakeShooter shooter, Vision vision,
+			Hang leftHanger, Hang rightHanger, ChassisDriveInputs inputs) throws Exception {
 		final Optional<Alliance> ally = DriverStation.getAlliance();
 
-		if (!vision.isEnabled()) throw new UnsupportedOperationException("This auto requires vision!");
-		if (ally.isEmpty()) throw new Exception("DriverStation.getAlliance is not present. Set the alliance in the driver station.");
+		if (!vision.isEnabled())
+			throw new UnsupportedOperationException("This auto requires vision!");
+		if (ally.isEmpty())
+			throw new Exception("DriverStation.getAlliance is not present. Set the alliance in the driver station.");
 
-		final int speakerTagId = ally.get() == Alliance.Red ? 4 : 7;
+		boolean redAlliance = ally.get() == Alliance.Red;
+		final int speakerTagId = redAlliance ? 4 : 7;
+		final Translation2d note3Translation = new Translation2d(
+				redAlliance ? rightNotePickup.getX() : -rightNotePickup.getY(),
+				rightNotePickup.getY());
 
 		return Commands.sequence(
-				shoot2StartingAuto(drivetrain, arm,shooter,leftHanger,rightHanger),
-				new AutoDriveTo(drivetrain, rightNotePickup)
-		);
+				shoot2StartingAuto(drivetrain, arm, shooter, leftHanger, rightHanger),
+				intakeFromFloorStart(arm, shooter),
+				new AutoDriveTo(drivetrain, note3Translation),
+				new AutoDriveTo(drivetrain, new Translation2d(0, note3Translation.getY())),
+				intakeFromFloorEnd(arm, shooter),
+				new AutoDriveTo(drivetrain, note3Translation.times(-1)),
+				new FollowTag(drivetrain, vision, speakerTagId,
+						new Translation2d(-speakerDepth - Constants.BOT_WIDTH / 2, 0)),
+				shootInSpeaker(drivetrain, arm, shooter, null, inputs));
+
 	}
 
 	public static Command dropInAmp(SwerveDrivetrain drivetrain, Arm arm, IntakeShooter shooter, Vision vision,
