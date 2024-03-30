8525b645d01b1b86922c416d7e53fbd425edd29f
Azalea H <aleahjham@gmail.com>
2024-03-30 15:53:43 -0700

    Update MoveToTagAndShootSpeaker.java

--- src/main/java/frc/robot/commands/MoveToTagAndShootSpeaker.java
+++ src/main/java/frc/robot/commands/MoveToTagAndShootSpeaker.java
@@ -45,12 +45,12 @@ public class MoveToTagAndShootSpeaker extends Command {
 				Translation2d transGoal = trans.getTranslation().toTranslation2d()
 						.plus(new Translation2d(-tagOffset, 0).rotateBy(rotateGoal.unaryMinus()));
 				Command command = Commands.sequence(
-						new SpinFlywheelShooter(shooter, IntakeShooterConstants.FLYWHEEL_SHOOTER_SPEED_SPEAKER),
+						new SpinFlywheelShooter(shooter, IntakeShooterConstants.FLYWHEEL_SHOOTER_SPEED_AMP),
 						Commands.parallel(Commands.sequence(new AutoRotateTo(drivetrain, rotateGoal),
 								new AutoDriveTo(drivetrain,
 										transGoal)),
 								new WaitCommand(1.0),
-								new ArmRotateTo(arm, ArmConstants.ARM_SPEAKER_SHOOTING_DEGREES),
+								new ArmRotateTo(arm, ArmConstants.ARM_AMP_SHOOTING_DEGREES),
 								new SpinIntakeGrabbers(shooter, IntakeShooterConstants.INTAKE_GRABBER_SPEED_SPEAKER),
 								new WaitCommand(0.3),
 								new SpinFlywheelShooter(shooter, 0.0),
